94175cc671aaf0491e0d2751fafaff4c69cda8d5
pravinprabhale22 <pravinprabhale009@gmail.com>
2022-03-22 08:32:54 +0530

    Updates

---
+++ precache-manifest.bbf1751d0d15eef204e825b00e095d74.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0080c4a3d43951d45d4be0f11270095",
+    "url": "/breaking-bad-cast/index.html"
+  },
+  {
+    "revision": "63817fb527b565eb9a31",
+    "url": "/breaking-bad-cast/static/css/main.6bae1351.chunk.css"
+  },
+  {
+    "revision": "c9413a2b0e288bcb1e85",
+    "url": "/breaking-bad-cast/static/js/2.68f4301c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/breaking-bad-cast/static/js/2.68f4301c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63817fb527b565eb9a31",
+    "url": "/breaking-bad-cast/static/js/main.b9ac98c5.chunk.js"
+  },
+  {
+    "revision": "aceb973de7feae88a752",
+    "url": "/breaking-bad-cast/static/js/runtime-main.039fb2eb.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/breaking-bad-cast/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/breaking-bad-cast/static/media/spinner.235b9b3e.gif"
+  }
+]);

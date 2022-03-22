50b0733ece23e2a58f0be48d6e0a9313bd16eb84
pravinprabhale22 <pravinprabhale009@gmail.com>
2022-03-22 08:31:55 +0530

    deploy my react app

---
+++ src/components/ui/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import logo from '../../img/logo.png.jpg'
+
+const Header = () => {
+  return (
+    <header className='center'>
+    <h1> Bad Charcters</h1>
+      <img src={logo} alt='' />
+    </header>
+  )
+}
+
+export default Header
